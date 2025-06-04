0454aefc796652dbdda443dae35d06b7757ee584
Alessandro Rutigliano <69083664+Lumingft18@users.noreply.github.com>
2025-06-04 18:09:03 +0200

    CORS

--- server-web/src/main/java/it/unimib/sd2025/CORSFilter.java
+++ server-web/src/main/java/it/unimib/sd2025/CORSFilter.java
@@ -10,12 +10,12 @@ import jakarta.ws.rs.ext.Provider;
  * Filtro per abilitare il CORS sulle risorse REST.
  */
 @Provider
-public class CORSFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "PUT, GET, HEAD, POST, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "*");
+        responseContext.getHeaders().add("Access-Control-Expose-Headers", "Location");
     }
 } 
