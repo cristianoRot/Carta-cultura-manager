e83f6f5f8598dad4042cf2299dc16393601f13f3
Cristiano <cristiano.rotunno@gmail.com>
2025-06-25 19:32:37 +0200

    Rimuovi commenti superflui nel file pom.xml e nella classe ApplicationConfig. Aggiorna il file index.html per migliorare la leggibilità del codice.

--- server-web/src/main/java/it/unimib/sd2025/ApplicationConfig.java
+++ server-web/src/main/java/it/unimib/sd2025/ApplicationConfig.java
@@ -5,19 +5,12 @@ import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 
 import jakarta.ws.rs.ApplicationPath;
 
-/**
- * Configurazione principale dell'applicazione REST.
- * Registra automaticamente tutte le risorse presenti nel package
- * e abilita la generazione della documentazione OpenAPI.
- */
 @ApplicationPath("/api")
 public class ApplicationConfig extends ResourceConfig {
 
-    public ApplicationConfig() {
-        // Effettua la scansione di tutte le classi presenti nel package di root
+    public ApplicationConfig() 
+    {
         packages("it.unimib.sd2025");
-
-        // Abilita la feature OpenAPI di Jersey che esporrà la spec su /openapi
         register(OpenApiResource.class);
     }
 } 
